279e97768292285a90cd9ad466e0d6bde827e472
Ryan Day <soldair@gmail.com>
2017-08-22 14:12:25 -0700

    no longer waits for each batch to finish

--- index.js
+++ index.js
@@ -4,130 +4,201 @@ var idleTimer = require('./lib/idle-timer')
 var json = require('./lib/json')
 var once = require('once')
 
-module.exports = function(opts,workfn){
-  opts = opts||{}
-  if(typeof opts === 'string') opts = {prefix:opts}
- 
+var EE = require('events').EventEmitter
 
-  var numAttempts = opts.attempts||3
-  var timeout = opts.timeout||0
+module.exports = function (opts, workfn) {
+  opts = opts || {}
+  if (typeof opts === 'string') opts = {prefix: opts}
+
+  var numAttempts = opts.attempts || 3
+  var timeout = opts.timeout || 0
   var prefix = opts.prefix
-
+  // opts.writeOnly
+  //
   var client = redis.createClient(opts.redis)
+
+  // assume we will connect.
+  var connected = true
+  client.on('connect', function () {
+    connected = true
+    queue.emit('metric', {name: 'redis-connected'})
+  }).on('end', function () {
+    // if there is an error connected etc end will be emitted.
+    // reconnecting will beemitted and if sucessful connect will be emitted again
+    connected = false
+    queue.emit('metric', {name: 'redis-disconnected'})
+  }).on('error', function (err) {
+    // without the retry_strategy redis option errors are emitted any time redis goes away
+    // but the client will still try to reconnect and process buffered commands.
+
+    queue.emit('metric', {name: 'redis-error'})
+    // i make a huge scary assumption here that of there is an error on this object the redis client will eventually fix it.
+  })
+
   var processing = false
   var ended = false
 
-  var queue = {
-    add:function(name,data,cb){
-      var multi = client.multi([
-        ['zadd',prefix+':set',Date.now(),name],
-        ['hset',prefix+':data',name,JSON.stringify({name:name,data:data})],
-      ])
-      multi.exec(function(err){
-        
-        boop()
-        cb(err)
-      });
-    },
-    end:function(){
-      ended = true
-      if(!processing) client.quit()
-      client = false
-    },
-    start:function(){
+  var max = opts.max || 10
+  var active = 0
 
-      if(!client) client = redis.createClient(opts.redis)
-      boop()
+  // var readOnlyInterval
 
-    },
-    //testing helper. fires as the next batch starts
-    batchbacks:[],
-    batchCallback:function(cb){
-      if(!processing) return setImmediate(cb)
-      this.batchbacks.push(cb)
-    },
-    client:client
+  var queue = new EE()
+  queue.add = function (name, data, cb) {
+    var multi = client.multi([
+      ['zadd', prefix + ':set', Date.now(), name],
+      ['hset', prefix + ':data', name, JSON.stringify({name: name, data: data})]
+    ])
+    multi.exec(function (err) {
+      assignWork()
+      cb(err)
+    })
   }
-  
 
-  if(opts.start !== false) {
-    boop()
+  queue.end = function () {
+    ended = true
+    if (!active && !processing) client.quit()
+    client = false
   }
+
+  queue.start = function () {
+    ended = false
+    if (!client) {
+      client = redis.createClient(opts.redis)
+      queue.client = client
+    }
+    assignWork()
+  }
+
+  queue.client = client
+
+  // if we are write only we dont start
+  if (!opts.writeOnly || opts.start !== false) {
+    assignWork()
+  }
+
+  // if this is a read only process we have to poll for new messages when we are idle.
+  // if(opts.readOnly) {
+  //  setInterval(assignWork,1000)
+  // }
 
   return queue
 
-  function boop(){
-    if(processing) return;
+  function assignWork () {
+    if (opts.writeOnly) return
+
+    var toAssign = max - active
+
+    if (!connected) {
+      return
+    }
+    if (processing) return
     processing = true
 
-    if(ended) return client.quit();
-    // flush actions waiting for batch to complete
-    while(queue.batchbacks.length) queue.batchbacks.shift()()
 
-    client.zrange(prefix+':set',0,10,function(err,data){
-      if(!data.length) {
-        processing = false
-        return;
+    getJobs(toAssign, function (err, jobs) {
+      processing = false
+      if (err) {
+        // if i cant get jobs from redis
+        return setTimeout(function () {
+          assignWork()
+        }, 1000)
       }
 
+      var start = Date.now()
 
-      client.hmget(prefix+':data',data,function(err,jobs){
-        //filter nulls
-        jobs = jobs.filter((job)=>job)
-        var todo = jobs.length
-        if(!todo) {
-          // should only hit this if the lists are corrupted. items are in the set but not in the map.
-          // if the data entries are deleted we can't ignore the matching items in the set.
-          // otherwise we'll get an error state where we wont contiue processing anything ever again.
-          client.zrem(prefix+':set',data,function(err){
-            // if err, and in this state we are up a creek
-            processing = false
-            boop()
+
+      if(!jobs.length) {
+        queue.emit('idle')
+      }
+
+      active += jobs.length
+      jobs.forEach(function (job) {
+        var next = once(function (err) {
+          if (timer) timer.clear()
+
+          queue.emit('metric', {name: 'job', value: Date.now() - start})
+
+          --active
+          if (ended && !active) {
+            client.quit()
+            client = false
+            return
+          }
+
+          assignWork()
+        })
+
+        // invalid json
+        if (!job) return next()
+
+        var timer
+        attempts(numAttempts, function (done) {
+          if (timer) timer.clear()
+          if (opts.timeout) {
+            timer = idleTimer(() => {
+              done(new Error('timeout. no callback after ' + opts.timeout + ' ms'))
+            }, opts.timeout)
+          }
+          workfn(job, function (err, data) {
+            done(err, data)
           })
-          return;
-        }
+        }, function (err) {
 
-        jobs.forEach(function(job){
-          job = json(job)
-          var timer;
+          if (timer) timer.clear()
+          var multi = client.multi()
+          if (err) {
+            multi.zadd(prefix + ':error', Date.now(), job.name)
+            multi.zrem(prefix + ':set', job.name)
+          } else {
+            multi.hdel(prefix + ':data', job.name)
+            multi.zrem(prefix + ':set', job.name)
 
-          var next = once(function(err){
-            if(timer) timer.clear()
-            if(!--todo) {
-              processing = false;
-              boop()
-            }           
+          }
+
+          multi.exec(function (err) {
+            queue.emit('metric', {name: 'redis-command-error'})
+            // if we get an error here we may continue to try and process this same set of jobs forever.
+            next(err)
           })
-
-          if(opts.timeout) timer = idleTimer(()=>next(new Error('timeout. no callback after '+opts.timeout+' ms')),opts.timeout)
-
-          attempts(numAttempts,function(done){
-            if(!job) return done(err)
-            if(timer) timer.activity()
-            workfn(job,done)
-          },function(err){
-            if(timer) timer.activity()
-            var multi = client.multi()
-            if(err){
-              multi.zadd(prefix+':error',Date.now(),job.name)
-              multi.zrem(prefix+':set',job.name)
-            } else {
-              multi.hdel(prefix+':data',job.name)
-              multi.zrem(prefix+':set',job.name)
-            }
-
-            multi.exec(function(rerr){
-              // handle redi error :'(
-              // if we get an error here we may continue to try and process this same set of jobs forever.
-              next()
-            });
-          });
-        });
-
+        })
       })
     })
   }
 
+  function getJobs (num, cb) {
+    var start = active
+    var end = active + num
+    client.zrange(prefix + ':set', start, end, function (err, data) {
+      if (err) return cb(err)
+      if (!data.length) {
+        return cb(null, [])
+      }
+
+      client.hmget(prefix + ':data', data, function (err, jobs) {
+        jobs.forEach(function (job, i) {
+          if (!job) return
+          if (job) job = json(job)
+          jobs[i] = job
+        })
+
+        // filter nulls
+        jobs = jobs.filter((job) => job)
+
+        var todo = jobs.length
+        if (!todo) {
+          // should only hit this if the lists are corrupted. items are in the set but not in the map.
+          // if the data entries are deleted we can't ignore the matching items in the set.
+          // otherwise we'll get an error state where we wont contiue processing anything ever again.
+          client.zrem(prefix + ':set', data, function (err) {
+            // if err, and in this state we are up a creek
+            cb(null, [])
+          })
+          return
+        }
+
+        cb(null, jobs)
+      })
+    })
+  }
 }
-
-
--- test/01-test.js
+++ test/01-test.js
@@ -9,6 +9,9 @@ test("can queue and process item",function(t){
   var datamap = qname+':data'
   var workset = qname+':set'
 
+  t.plan(11)
+
+  var called = 0
   var queue = todoQueue(qname,function workfn(data,cb){
     t.equals(data.name,'blorp','should have name')
     t.equals(data.data.data,1,'should have correct data')
@@ -16,6 +19,8 @@ test("can queue and process item",function(t){
     client = queue.client
 
     client.hget(datamap,'blorp',function(err,data){
+      t.ok(++called <= 1,'should only have to do one attempt!')
+
       t.ok(!err)
       t.ok(data,'should have data')
       t.equals(JSON.parse(data).name,'blorp','should be named blorp. shoudl be where i expectr it in the queue')
@@ -28,8 +33,10 @@ test("can queue and process item",function(t){
   })
 
 
-  queue.add('blorp',{data:1},function(){
-    queue.batchCallback(function(){
+  queue.add('blorp',{data:1},function(err){
+    t.ok(!err,'do not have error adding blorp')
+
+    queue.once('idle',function(){
       var client = queue.client
     
       client.hget(datamap,'blorp',function(err,data){
@@ -37,13 +44,15 @@ test("can queue and process item",function(t){
         t.ok(!data,'should not have data')
         client.zrange(workset,0,1,function(err,keys){
           t.equals(keys.length,0,'should have 0 keys.')
+
           queue.end()
-          t.end()
         }) 
-      })
-      
+      })  
     })
   })
 
 })
+
+
+
 
